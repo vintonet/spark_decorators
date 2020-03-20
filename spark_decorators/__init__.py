678e9a17122c05c9e94fec11dbba990e595460b3
moss <luke.vinton@microsoft.com>
2020-03-20 12:52:12 +0000

    removed unnecessary reference

--- spark_decorators/__init__.py
+++ spark_decorators/__init__.py
@@ -1,3 +1,2 @@
-from pyspark import SparkContext
 from spark_decorators.selector import SelectorConf, SelectorInput, SelectorOutput, execute_selector, display_selector
 from spark_decorators.plan import Plan
