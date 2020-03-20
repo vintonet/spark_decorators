ae52c5ab5ae969c9990c9b2bea13227ab4fd5531
moss <luke.vinton@microsoft.com>
2020-03-20 11:23:27 +0000

    refactored logic to create and execute plans

--- spark_decorators/plan.py
+++ spark_decorators/plan.py
@@ -1,21 +1,30 @@
 import spark_decorators.selector as S
 import json
+from typing import Dict
+
+class Stage(object):
+    name: str 
+    arguments: dict
+    def __init__(self, name, arguments):
+        self.name = name
+        self.arguments = arguments
+
 
 class Plan(object):
     name: str
-    stages: dict = {}
+    stages: Dict[int, Stage] = {}
     def __init__(self, name: str):
         self.name = name
         
     def __repr__(self):
-        print_dict = {}
-        for key, value in self.stages.items():
-            print_dict[key] = str(value)
-        return json.dumps(print_dict)
+        return json.dumps({
+            "name": self.name,
+            "stages": self.stages
+        })
 
-    def add_stage(self, s: S.Selector, arg_dict: dict):
-        s.arg_dict = arg_dict 
-        self.stages[len(self.stages)+1] = s
+    def add_stage(self, name: str, arguments: dict):
+        stage = Stage(name, arguments)
+        self.stages[len(self.stages)+1] = stage
 
     def execute(self, **kwargs) -> DataFrame:
         stage_numbers = []
@@ -25,13 +34,14 @@ class Plan(object):
             stage_numbers = range(1,len(self.stages)+1)
         stage_dataframes = {}
         for idx, stage_number in enumerate(stage_numbers):
-            stage_selector: S.Selector = self.stages[stage_number]
-            if stage_selector:
+            stage = self.stages[stage_number]
+            stage_selector = S.selector_registry[stage.name]
+            if stage and S.selector_registry[stage.name]:
                 if stage_selector.conf.in_type == S.SelectorInput.NONE:
                     stage_dataframes[stage_number] = S._execute_selector(stage_selector)
                 else:
                     in_df = stage_dataframes[stage_numbers[idx-1]]
-                    stage_dataframes[stage_number] = S._execute_selector(stage_selector, in_df)
+                    stage_dataframes[stage_number] = S._execute_selector(stage_selector, in_df = in_df)
         return stage_dataframes[stage_numbers[-1]]
         
 
--- spark_decorators/selector/decorator.py
+++ spark_decorators/selector/decorator.py
@@ -15,28 +15,30 @@ def selector(c: SelectorConf):
 
 def execute_selector(name: str, **kwargs) -> DataFrame:
     s: Selector = selector_registry[name]
+    return _execute_selector(s, **kwargs)
+
+def _execute_selector(s: Selector, **kwargs) -> DataFrame:
     s.conf.arg_names.extend(in_args[s.conf.in_type.name])
     s.conf.arg_names.extend(out_args[s.conf.out_type.name])
     s.arg_dict = {**s.arg_dict, **kwargs}
     missing_args = [key for key, value in s.arg_dict if key not in s.conf.arg_names]
     if len(missing_args) > 0:
         raise Exception(f"Missing required arguments: {' '.join(missing_args)}")
-    in_df = in_resolvers[s.conf.in_type.name](s)
-    return _execute_selector(s, in_df)
-
-def _execute_selector(s: Selector, **kwargs) -> DataFrame:
-    in_df: DataFrame
     out_df: DataFrame
     if s.conf.in_type.NONE:
         out_df = s.func(**s.arg_dict)
     else:
+        in_df: DataFrame
         if kwargs["in_df"]:
             in_df = kwargs["in_df"]
         else:
-            raise Exception("Selector requires an input dataframe, but one wasn't provided")
+            in_df = in_resolvers[s.conf.in_type.name](s)
         out_df = s.func(in_df, **s.arg_dict)
     out_resolvers[s.conf.out_type.name](s, out_df)
     return out_df
 
 def register_spark_context(sc):
     spark_context = sc
+
+def display_selector(name):
+    return str(selector_registry[name])
