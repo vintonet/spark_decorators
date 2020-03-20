7e2d068ee4fb925c161a5d602880e3d0c64d1af2
moss <luke.vinton@microsoft.com>
2020-03-20 15:18:19 +0000

    add missing import

--- spark_decorators/__init__.py
+++ spark_decorators/__init__.py
@@ -1,3 +1,3 @@
 from .plan import Plan, Stage
-from .decorator import _execute_selector, execute_selector, display_selector
+from .decorator import _execute_selector, execute_selector, display_selector, selector
 from .selector import SelectorConf, SelectorInput, SelectorOutput
